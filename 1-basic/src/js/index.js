d9dbaa40f77bd9aed6516505efbdad9e13485795
Rajesh Naroth <rajeshnaroth@rajeshspro1986.attlocal.net>
2018-02-16 20:07:24 -0800

    Initial Commit

---
+++ 1-basic/src/js/index.js
@@ -0,0 +1,8 @@
+"use strict"
+
+import React from "react"
+import ReactDOM from "react-dom"
+
+import Title from './title'
+
+ReactDOM.render(<Title />, document.getElementById('app'))
---
+++ 1-basic/src/js/title.js
@@ -0,0 +1,6 @@
+"use strict"
+import React from "react"
+
+const Title = () => <h1> Hello Superman</h1>
+
+export default Title
